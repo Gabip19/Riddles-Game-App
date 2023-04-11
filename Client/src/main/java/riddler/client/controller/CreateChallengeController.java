76ce3bfe6d7899251ea70ba49d703e0b5cf53036
Gabip19 <gabrielpaslaru01@gmail.com>
2023-04-12 02:00:09 +0300

    create challenge menu design done

--- Client/src/main/java/riddler/client/controller/CreateChallengeController.java
+++ Client/src/main/java/riddler/client/controller/CreateChallengeController.java
@@ -1,6 +1,7 @@
 package riddler.client.controller;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import riddler.domain.Challenge;
@@ -16,25 +17,35 @@ public class CreateChallengeController extends GuiController {
     public TextField tokensPoolField;
     public TextField tokensPrizeField;
     public Button addChallengeBtn;
+    public Label resultLabel;
 
 
     public void initialize() {
         addChallengeBtn.setOnAction(param -> addChallenge());
+        resultLabel.setVisible(false);
     }
 
     private void addChallenge() {
         try {
-            String title = titleField.getText();
-            String text = challengeTextField.getText();
-            String answer = answerField.getText();
-            int maxAttempts = Integer.parseInt(maxAttemptsField.getText());
-            int badgesPrizePool = Integer.parseInt(badgesPoolField.getText());
-            int tokensPrizePool = Integer.parseInt(tokensPoolField.getText());
-            int tokensPrize = Integer.parseInt(tokensPrizeField.getText());
+            resultLabel.setVisible(false);
 
             if (hasEmptyFields()) {
                 return;
             }
+
+            String title = titleField.getText();
+            String text = challengeTextField.getText();
+            String answer = answerField.getText();
+            int maxAttempts;
+            if (maxAttemptsField.getText().equals("no")) {
+                maxAttempts = Challenge.INFINITE_ATTEMPTS;
+            } else {
+                maxAttempts = Integer.parseInt(maxAttemptsField.getText());
+            }
+            int badgesPrizePool = Integer.parseInt(badgesPoolField.getText());
+            int tokensPrizePool = Integer.parseInt(tokensPoolField.getText());
+            int tokensPrize = Integer.parseInt(tokensPrizeField.getText());
+
 
             Challenge challenge = new Challenge(
                     title,
@@ -48,11 +59,29 @@ public class CreateChallengeController extends GuiController {
             );
 
             srv.addChallenge(challenge);
+            clearFields();
+            resultLabel.setText("Succes");
+            resultLabel.setStyle("-fx-text-fill: #3eea3e");
+            resultLabel.setVisible(true);
         } catch (NumberFormatException e) {
-            System.out.println("Not a number.\n");
+            resultLabel.setText("Not a number.\n");
+            resultLabel.setStyle("-fx-text-fill: red");
+            resultLabel.setVisible(true);
         } catch (ChallengeValidationException e) {
-            System.out.println(e.getMessage());
+            resultLabel.setText(e.getMessage());
+            resultLabel.setStyle("-fx-text-fill: red");
+            resultLabel.setVisible(true);
         }
+    }
+
+    private void clearFields() {
+        titleField.clear();
+        challengeTextField.clear();
+        answerField.clear();
+        maxAttemptsField.clear();
+        badgesPoolField.clear();
+        tokensPoolField.clear();
+        tokensPrizeField.clear();
     }
 
     private boolean hasEmptyFields() {
